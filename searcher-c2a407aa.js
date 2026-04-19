b9c889f9a287182b9a04ea2509b22dbc35694782
windmissing <windmissing@users.noreply.github.com>
2026-04-19 12:34:25 +0000

    deploy: fdd52a0bac9110fbde70cfacdb8aa4659c308d46

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2ceb1d71.js',
+                path_to_root + 'searchindex-0edf8d6b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
